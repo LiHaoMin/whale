01dd130d2712576e1b639d78c5f3b3e0fbfd6adf
lihaomin <lihaomin666@qq.com>
2021-01-26 17:36:48 +0800

    api

--- whale-api/src/main/java/li/haomin/whale/api/MediaController.java
+++ whale-api/src/main/java/li/haomin/whale/api/MediaController.java
@@ -1,10 +1,15 @@
 package li.haomin.whale.api;
 
+import li.haomin.whale.model.Media;
+import li.haomin.whale.model.MediaUri;
 import li.haomin.whale.repository.MediaRepository;
+import li.haomin.whale.repository.MediaUriRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/media")
@@ -12,9 +17,50 @@ public class MediaController {
 
     @Autowired
     private MediaRepository mediaRepository;
+    @Autowired
+    private MediaUriRepository mediaUriRepository;
 
     @GetMapping("/total")
     public Long total() {
         return mediaRepository.count();
+    }
+
+    @GetMapping("/page")
+    public Page<Media> page(@RequestParam(required = false) String tag,
+                            @RequestParam(required = false) String area,
+                            @RequestParam(defaultValue = "0") Integer page,
+                            @RequestParam(defaultValue = "10") Integer size,
+                            @RequestParam(defaultValue = "time") String sort,
+                            @RequestParam(defaultValue = "desc") String dir) {
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.fromString(dir), sort));
+        Media media = new Media();
+        if (tag != null && !tag.isEmpty()) media.setTag(tag);
+        if (area != null && !area.isEmpty()) media.setArea(area);
+        Page<Media> pages = mediaRepository.findAll(Example.of(media), pageable);
+        return pages;
+    }
+
+    @GetMapping("/{id}")
+    public Media media(@PathVariable Long id) {
+        Optional<Media> media = mediaRepository.findById(id);
+        if (media.isPresent()) {
+            List<MediaUri> mediaUris = mediaUriRepository.findAllByMediaId(id);
+            media.get().setMediaUris(mediaUris);
+            return media.get();
+        }
+        return null;
+    }
+
+    @GetMapping("/tags")
+    public List<String> tags() {
+
+        return mediaRepository.findAllTags();
+    }
+
+    @GetMapping("/areas")
+    public List<String> areas() {
+
+        return mediaRepository.findAllAreas();
     }
 }
--- whale-common/src/main/java/li/haomin/whale/repository/MediaRepository.java
+++ whale-common/src/main/java/li/haomin/whale/repository/MediaRepository.java
@@ -2,10 +2,19 @@ package li.haomin.whale.repository;
 
 import li.haomin.whale.model.Media;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MediaRepository extends JpaRepository<Media, Long> {
 
     Media findFirstByName(String name);
+
+    @Query(nativeQuery = true, value="SELECT tag FROM media GROUP BY tag ORDER BY tag")
+    List<String> findAllTags();
+
+    @Query(nativeQuery = true, value="SELECT area FROM media GROUP BY area ORDER BY area")
+    List<String> findAllAreas();
 }
