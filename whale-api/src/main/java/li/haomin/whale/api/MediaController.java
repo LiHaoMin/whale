2e4ee31ae20edfc6ef18e8ed8e0777a1772bf24f
lihaomin <lihaomin666@qq.com>
2021-02-02 16:46:37 +0800

    api

--- whale-api/src/main/java/li/haomin/whale/api/MediaController.java
+++ whale-api/src/main/java/li/haomin/whale/api/MediaController.java
@@ -28,16 +28,19 @@ public class MediaController {
     @GetMapping("/page")
     public Page<Media> page(@RequestParam(required = false) String tag,
                             @RequestParam(required = false) String area,
+                            @RequestParam(required = false) String name,
                             @RequestParam(defaultValue = "0") Integer page,
                             @RequestParam(defaultValue = "10") Integer size,
                             @RequestParam(defaultValue = "time") String sort,
                             @RequestParam(defaultValue = "desc") String dir) {
 
         Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.fromString(dir), sort));
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching().withMatcher("name", match -> match.contains());
         Media media = new Media();
         if (tag != null && !tag.isEmpty()) media.setTag(tag);
         if (area != null && !area.isEmpty()) media.setArea(area);
-        Page<Media> pages = mediaRepository.findAll(Example.of(media), pageable);
+        if (name != null && !name.isEmpty()) media.setName(name);
+        Page<Media> pages = mediaRepository.findAll(Example.of(media, exampleMatcher), pageable);
         return pages;
     }
 
